c50341cf5e88b23b7b427452d1c8ecb980b56a2b
ynussmert06 <251485177+ynussmert06@users.noreply.github.com>
2026-01-10 23:10:43 +0300

    игра против пк есть,попробую сделать в обратную

--- game.py
+++ game.py
@@ -1,27 +1,23 @@
 import random
-game = input("выберите режи игры:  человек угадывает: chel, компьютер угадывает: pk")
+
 r = 0
 score = 0
 
-if game == "chel":
-    level = input("уровень сложности: easy = 0-100, middle = 0-500, hard = 0-1000 ")
-    chislo = int(input("компьютер загадал число, сможешь угадать?: "))
-    # воспользовался словарем чтобы лучше закрепить, а так можно просто через if
-    operachion = {
-        "easy": lambda r: r + random.randint(0, 100),
-        "middle": lambda r: r + random.randint(0, 500), 
-        "hard": lambda r: r + random.randint(0, 1000)}
-    if level in operachion:
-        random_ = operachion[level](r)
-    while chislo != random_:
-        if chislo > random_:
-            score+=1
-            chislo = int(input("не уагадал,число меньше, попробуй еще раз: "))
-        if chislo < random_:
-            score+=1
-            chislo = int(input("не уагадал, число больше, попробуй еще раз: "))
-    print(f"ты угадал", "количество попыток:", score+1)
-if game == "pk":
-    chislo_pk = int(input("загадайте число компьютеру: "))
-    while chislo_pk != r:
-        
+
+level = input("уровень сложности: easy = 0-100, middle = 0-500, hard = 0-1000 ")
+chislo = int(input("компьютер загадал число, сможешь угадать?: "))
+        # воспользовался словарем чтобы лучше закрепить, а так можно просто через if
+operachion = {
+            "easy": lambda r: r + random.randint(0, 100),
+            "middle": lambda r: r + random.randint(0, 500), 
+            "hard": lambda r: r + random.randint(0, 1000)}
+if level in operachion:
+            random_ = operachion[level](r)
+while chislo != random_:
+            if chislo > random_:
+                score+=1
+                chislo = int(input("не уагадал,число меньше, попробуй еще раз: "))
+            if chislo < random_:
+                score+=1
+                chislo = int(input("не уагадал, число больше, попробуй еще раз: "))
+print(f"ты угадал", "количество попыток:", score+1)
