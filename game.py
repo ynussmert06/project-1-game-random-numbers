171a38f0f6afe3d98d1896d50009eb5168f66c55
ynussmert06 <251485177+ynussmert06@users.noreply.github.com>
2026-01-19 01:04:53 +0300

    add sqlite, add table_score, add TOP 5 and delete where < top 5, исправил ошибку если ввести уровень то не запускалось

--- game.py
+++ game.py
@@ -1,4 +1,25 @@
 import random
+import sqlite3
+# создадим базу данный для сохранения рекордов
+table = sqlite3.connect("record_table")
+cursorr = table.cursor()
+human_table = ("""CREATE TABLE IF NOT EXISTS chel_record(
+                name_player text,
+                lvl text,
+                score_player integer
+                )""")
+pk_table =("""CREATE TABLE IF NOT EXISTS pk_record(
+                name_player text,
+                score_pk integer
+                )""")
+cursorr.execute(human_table)
+cursorr.execute(pk_table)
+table.commit()
+
+
+
+
+name = input("введите имя игрока: ")
 game = input("выберите режи игры:  человек угадывает: chel, компьютер угадывает: pk ")
 if game not in ["chel", "pk"]:
     game = random.choice(["chel","pk"])
@@ -10,6 +31,7 @@ if game == "chel":
     if level not in ["easy","middle","hard"]:
         level = random.choice(["easy","middle","hard"])
         print("неопределенный ввод, программа выбрала сложность", level)
+    else:
         while True:
             try:
                 chislo = int(input("компьютер загадал число, сможешь угадать?: "))
@@ -27,16 +49,25 @@ if game == "chel":
                         if chislo < random_:
                             score+=1
                             chislo = int(input("не угадал, число больше, попробуй еще раз: "))
+               
                 print("ты угадал,", "количество попыток:", score+1)
+                cursorr.execute('INSERT INTO chel_record VALUES(?,?,?)', (name, level, score+1))
+                cursorr.execute('SELECT * FROM chel_record ORDER BY score_player DESC LIMIT 5')
+                chel = cursorr.fetchall()
+                print("-----------ТАБЛИЦА----------")
+                for a, row in enumerate(chel):
+                    print(f"TOP {a+1} --> {row}")
+                cursorr.execute('DELETE FROM chel_record WHERE score_player < (SELECT MIN(score_player) FROM chel_record ORDER BY score_player DESC LIMIT 5)')
+                table.commit()
+                table.close()
                 break
-
             except ValueError:
                  print("введенно не число, повторите попытку")
    
 if game == "pk":
     mini = 0
     maxi = 1000
-    score_pk = 0
+    score_pk_game = 0
     while True:
          try:
             chislo_pk = int(input("загадайте число компьютеру До 1000: "))
@@ -50,12 +81,22 @@ if game == "pk":
     pk = random.randint(mini, maxi)
     while chislo_pk != pk:
         if pk > chislo_pk:
-            score_pk+=1
+            score_pk_game+=1
             min = pk+1
             pk = random.randint(mini, maxi)
         if pk < chislo_pk:
-            score_pk+=1
+            score_pk_game+=1
             max = pk-1
-            pk = random.randint(mini, maxi)
-        
-    print("комп угадал твое число:", chislo_pk, "за", score_pk+1 ,"попыток")
+            pk = random.randint(mini, maxi) 
+    print("комп угадал твое число:", chislo_pk, "за", score_pk_game+1 ,"попыток")
+    cursorr.execute('INSERT INTO pk_record VALUES(?,?)', (name, score_pk_game+1))
+    cursorr.execute('SELECT * FROM pk_record ORDER BY score_pk DESC LIMIT 5')
+    
+    pk_ = cursorr.fetchall()
+    print("---------ТАБЛИЦА---------")
+    for i, roe in enumerate(pk_):
+         print(f"TOP {i+1} -->{roe}")
+    cursorr.execute('DELETE FROM pk_record WHERE score_pk < (SELECT MIN(score_pk) FROM pk_record ORDER BY score_pk DESC LIMIT 5)')
+    table.commit()
+    table.close()
+table.close()
